e769eb901fcfd7850ee9889c8508062842f9df9e
AnvarNurmanov <anvarnurmanov31@gmail.com>
2025-12-11 13:15:44 +0300

    промежуточный результат

---
+++ assets/scripts/script.js
@@ -0,0 +1,41 @@
+const headerToggel = document.querySelector("#header__phone");
+const arrowDown = document.querySelector("#arrow__down");
+const headerModal = document.querySelector("#header__modal");
+const modal = document.querySelector("#modal__overlay");
+const body = document.querySelector("body");
+
+headerToggel.addEventListener('click', ()=>{
+    modal.style.display = "block";
+    arrowDown.style.transform = 'rotate(180deg)';
+    headerModal.classList.add("header__modal__active");
+});
+
+modal.addEventListener('click', ()=>{
+    modal.style.display = "none";
+arrowDown.style.transform = 'rotate(0deg)';
+    headerModal.classList.remove("header__modal__active")
+});
+
+
+const burgerBut = document.querySelector("#burger__but");
+
+burgerBut.addEventListener('click', ()=>{
+    // const burgerButActive = document.querySelector(".burger__but__active");
+    // console.log(burgerButActive);
+    
+    if (burgerBut.classList.contains("burger__but__active")) {
+        body.style.overflowY = "";
+            burgerBut.classList.remove("burger__but__active")
+            const headerMenu = document.querySelector(".header__menu__active");
+            headerMenu.classList.remove("header__menu__active");
+    }else{
+        body.style.overflowY = "hidden";
+        burgerBut.classList.add("burger__but__active")
+        const headerMenu = document.querySelector("#header__menu");
+        headerMenu.classList.add("header__menu__active");
+        
+    }
+})
+
+// }
+
